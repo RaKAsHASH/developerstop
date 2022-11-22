f938ed7399b2fb669a863f50239aaf62bfb15ad4
Harjeet <theonlyking135@gmail.com>
2022-11-22 13:50:42 +0530

    tried .env fifth commit

--- final_project/settings.py
+++ final_project/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -143,9 +144,10 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ="smtp.gmail.com"
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER=config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
-
+EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
+print(EMAIL_HOST_USER)
+EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
+print(EMAIL_HOST_PASSWORD)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
